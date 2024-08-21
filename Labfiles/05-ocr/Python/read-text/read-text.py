757ac33462850720fb0c0a38d0048aabfe1ee0f0
Faris Shatat <farisshatat4@gmail.com>
2024-08-20 20:25:23 -0700

    updates

--- Labfiles/05-ocr/Python/read-text/read-text.py
+++ Labfiles/05-ocr/Python/read-text/read-text.py
@@ -5,12 +5,15 @@ from PIL import Image, ImageDraw
 from matplotlib import pyplot as plt
 
 # Import namespaces
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
 
     global cv_client
-
+    global command;
+    
     try:
         # Get Configuration Settings
         load_dotenv()
@@ -18,7 +21,7 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Authenticate Azure AI Vision client
-        
+        cv_client = ImageAnalysisClient(endpoint=ai_endpoint, credential=AzureKeyCredential(ai_key))
 
         # Menu for text reading functions
         print('\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n')
@@ -28,6 +31,9 @@ def main():
             GetTextRead(image_file)
         elif command =='2':
             image_file = os.path.join('images','Note.jpg')
+            GetTextRead(image_file)
+        elif command == '3':
+            image_file = os.path.join('images','stylophone.JPG')
             GetTextRead(image_file)
                 
 
@@ -42,8 +48,53 @@ def GetTextRead(image_file):
             image_data = f.read()
 
     # Use Analyze image function to read text in image
-    
-    
+    result = cv_client.analyze(
+        image_data=image_data,
+        visual_features=[VisualFeatures.READ]
+    )   
+    # Display the image and overlay it with the extracted text
+    if result.read is not None:
+        print("\nText:")
+    # Prepare image for drawing
+    image = Image.open(image_file)
+    fig = plt.figure(figsize=(image.width/100, image.height/100))
+    plt.axis('off')
+    draw = ImageDraw.Draw(image)
+    color = 'blue'
+
+    for line in result.read.blocks[0].lines:
+        # Return the text detected in the image
+        print(f"  {line.text}")    
+
+        drawLinePolygon = True
+
+        r = line.bounding_polygon
+        bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y),(r[3].x, r[3].y))
+
+        # Return the position bounding box around each line
+        print("  Bounding box: {}".format(bounding_polygon))
+        drawWordPolygon = command if command == '3' else False
+        
+        if drawWordPolygon:
+            # Return each word detected in the image and the position bounding box around each word with the confidence level of each word
+            for word in line.words:
+                r = word.bounding_polygon
+                bounding_polygon = ((r[0].x, r[0].y),(r[1].x, r[1].y),(r[2].x, r[2].y),(r[3].x, r[3].y))
+                print(f"    Word: '{word.text}', Bounding Polygon: {bounding_polygon}, Confidence: {word.confidence:.4f}")
+                # Draw word bounding polygon
+                drawLinePolygon = True
+                draw.polygon(bounding_polygon, outline=color, width=10)
+
+        # Draw line bounding polygon
+        if drawWordPolygon == False:
+            draw.polygon(bounding_polygon, outline=color, width=3)
+
+    # Save image
+    plt.imshow(image)
+    plt.tight_layout(pad=0)
+    outputfile = 'text.jpg'
+    fig.savefig(outputfile)
+    print('\n  Results saved in', outputfile)
 
 
 
